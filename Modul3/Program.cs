0ad4b400a9b2a7e7a1f36f229331b52ce81af974
Dsean <dushen.ulanov@gmail.com>
2022-11-24 12:29:26 +0600

    Молдуль 3 итог

--- Modul3/Program.cs
+++ Modul3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+        Console.Write("Enter u name: ");
+        var name = Console.ReadLine();
+
+        Console.Write("Enter u age: ");
+        var age = checked((byte)int.Parse(Console.ReadLine()));
+
+        Console.WriteLine($"Your name is {name} and age is {age} ");
+
+        Console.Write("Enter u birthdate: ");
+        var birthdate = Console.ReadLine();
+
+        Console.WriteLine($"Your birthdate is {birthdate}");
+
+
